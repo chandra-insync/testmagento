e5486e104b209ce5d09fb1336e600553b8525545
ChandanPradhan <chandan25pradhan@gmail.com>
2020-04-27 10:45:11 +0530

    added block file

---
+++ app/code/Chandan/SalesOrderMessageCustomerAccount/Block/Account/Dashboard/Info.php
@@ -0,0 +1,87 @@
+<?php
+namespace Appseconnect\CustomerAccount\Block\Account\Dashboard;
+
+use Magento\Framework\Exception\NoSuchEntityException;
+
+class Info extends \Magento\Customer\Block\Account\Dashboard\Info
+{
+    /**
+     * @var \Magento\Sales\Model\ResourceModel\Order\CollectionFactory
+     */
+    protected $_orderCollectionFactory;
+    /**
+     * @var \Magento\Customer\Model\Session
+     */
+    protected $_customerSession;
+    /**
+     * @var \Magento\Sales\Model\Order\Config
+     */
+    protected $_orderConfig;
+    /**
+     * @var \Magento\Sales\Api\OrderRepositoryInterface
+     */
+    protected $orderRepository;
+
+    /**
+     * Info constructor.
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param \Magento\Customer\Helper\Session\CurrentCustomer $currentCustomer
+     * @param \Magento\Newsletter\Model\SubscriberFactory $subscriberFactory
+     * @param \Magento\Customer\Helper\View $helperView
+     * @param \Magento\Sales\Model\ResourceModel\Order\CollectionFactory $orderCollectionFactory
+     * @param \Magento\Customer\Model\Session $customerSession
+     * @param \Magento\Sales\Model\Order\Config $orderConfig
+     * @param \Magento\Sales\Api\OrderRepositoryInterface $orderRepository
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Customer\Helper\Session\CurrentCustomer $currentCustomer,
+        \Magento\Newsletter\Model\SubscriberFactory $subscriberFactory,
+        \Magento\Customer\Helper\View $helperView,
+        \Magento\Sales\Model\ResourceModel\Order\CollectionFactory $orderCollectionFactory,
+        \Magento\Customer\Model\Session $customerSession,
+        \Magento\Sales\Model\Order\Config $orderConfig,
+        \Magento\Sales\Api\OrderRepositoryInterface $orderRepository,
+        array $data = []
+    )
+    {
+        $this->_orderCollectionFactory = $orderCollectionFactory;
+        $this->_customerSession = $customerSession;
+        $this->orderRepository = $orderRepository;
+        $this->_orderConfig = $orderConfig;
+        parent::__construct(
+            $context,
+            $currentCustomer,
+            $subscriberFactory,
+            $helperView,
+            $data
+        );
+    }
+
+    public function getOrders()
+    {
+        if (!($customerId = $this->_customerSession->getCustomerId())) {
+            return false;
+        }
+        $ordercollection= $this->_orderCollectionFactory->create()
+            ->addAttributeToSelect('*')
+            ->addFieldToFilter('customer_id', $customerId)
+            ->addFieldToFilter(
+                'status',
+                ['in' => $this->_orderConfig->getVisibleOnFrontStatuses()]
+            )->setOrder(
+                'created_at',
+                'desc'
+            );
+        return $ordercollection;
+    }
+    public function getOrderItems($order_id)
+    {
+        return $this->orderRepository->get($order_id);
+    }
+    public function getTemplate()
+    {
+        return 'account/dashboard/info.phtml';
+    }
+}
