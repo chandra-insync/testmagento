9845269b9569ad86c5a8cb0a57803a3d8a6d9876
ChandanPradhan <chandan25pradhan@gmail.com>
2020-04-27 18:47:26 +0530

    added logic for b2b customer in block file

--- app/code/Chandan/SalesOrderMessageCustomerAccount/Block/Account/Dashboard/Info.php
+++ app/code/Chandan/SalesOrderMessageCustomerAccount/Block/Account/Dashboard/Info.php
@@ -21,6 +21,12 @@ class Info extends \Magento\Customer\Block\Account\Dashboard\Info
      * @var \Magento\Sales\Api\OrderRepositoryInterface
      */
     protected $orderRepository;
+    /**
+     * @var \Appseconnect\B2BMage\Helper\ContactPerson\Data
+     */
+    public $helperContactPerson;
+
+    protected $orders;
 
     protected $total;
 
@@ -32,6 +38,7 @@ class Info extends \Magento\Customer\Block\Account\Dashboard\Info
      * @param \Magento\Customer\Helper\View $helperView
      * @param \Magento\Sales\Model\ResourceModel\Order\CollectionFactory $orderCollectionFactory
      * @param \Magento\Customer\Model\Session $customerSession
+     * @param \Appseconnect\B2BMage\Helper\ContactPerson\Data $helperContactPerson
      * @param \Magento\Sales\Model\Order\Config $orderConfig
      * @param \Magento\Sales\Api\OrderRepositoryInterface $orderRepository
      * @param array $data
@@ -43,6 +50,7 @@ class Info extends \Magento\Customer\Block\Account\Dashboard\Info
         \Magento\Customer\Helper\View $helperView,
         \Magento\Sales\Model\ResourceModel\Order\CollectionFactory $orderCollectionFactory,
         \Magento\Customer\Model\Session $customerSession,
+        \Appseconnect\B2BMage\Helper\ContactPerson\Data $helperContactPerson,
         \Magento\Sales\Model\Order\Config $orderConfig,
         \Magento\Sales\Api\OrderRepositoryInterface $orderRepository,
         array $data = []
@@ -50,6 +58,7 @@ class Info extends \Magento\Customer\Block\Account\Dashboard\Info
     {
         $this->_orderCollectionFactory = $orderCollectionFactory;
         $this->_customerSession = $customerSession;
+        $this->helperContactPerson = $helperContactPerson;
         $this->orderRepository = $orderRepository;
         $this->_orderConfig = $orderConfig;
         parent::__construct(
@@ -60,24 +69,47 @@ class Info extends \Magento\Customer\Block\Account\Dashboard\Info
             $data
         );
     }
-
     public function getOrders()
     {
         $this->total=[];
         if (!($customerId = $this->_customerSession->getCustomerId())) {
             return false;
         }
-        $ordercollection= $this->_orderCollectionFactory->create()
-            ->addAttributeToSelect('*')
-            ->addFieldToFilter('customer_id', $customerId)
-            ->addFieldToFilter(
-                'status',
-                ['in' => $this->_orderConfig->getVisibleOnFrontStatuses()]
-            )->setOrder(
-                'created_at',
-                'desc'
-            );
-        return $ordercollection;
+        $contactPersonRole = null;
+        $salesrepId = null;
+        $contactId = [];
+        $customerType = $this->_customerSession->getCustomer()->getCustomerType();
+        if ($customerType == 2) {
+            $salesrepId = $customerId;
+        } elseif ($customerType == 3) {
+            $contactPersonId = $this->_customerSession->getCustomerId();
+            $contactPersonRole = $this->_customerSession->getCustomer()->getContactpersonRole();
+            $parentCustomerMapData = $this->helperContactPerson->getCustomerId($contactPersonId);
+            $contactId[] = $parentCustomerMapData['customer_id'];
+        }
+        if (!$this->orders) {
+            if ($contactPersonRole == 2) {
+                $orderCollection = $this->_orderCollectionFactory->create()
+                    ->addFieldToFilter('contact_person_id', $contactPersonId);
+            } elseif ($salesrepId) {
+                $orderCollection = $this->_orderCollectionFactory->create()
+                    ->addFieldToFilter('salesrep_id', $salesrepId);
+            } elseif ($contactId) {
+                $orderCollection = $this->_orderCollectionFactory->create()
+                    ->addFieldToFilter('customer_id', [
+                        'in' => $contactId
+                    ]);
+            } else {
+                $orderCollection = $this->_orderCollectionFactory->create()
+                    ->addFieldToFilter('customer_id', $customerId);
+            }
+            $this->orders = $orderCollection->addFieldToSelect('*')
+                ->addFieldToFilter('status', [
+                    'in' => $this->_orderConfig->getVisibleOnFrontStatuses()
+                ])
+                ->setOrder('created_at', 'desc');
+        }
+        return $this->orders;
     }
     public function getOrderItems($order_id)
     {
